7dbd06530433763b1931f6d991425ef255640bb6
Rahul Kalra <rhlkalra50@gmail.com>
2023-08-01 13:51:43 +0530

    done changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,14 @@ import { Link } from "react-router-dom";
 
 export default function Navbar(props) {
   return (
-    <header
-      className="header px-4 py-3 position-fixed transition"
-    >
+    <header className="header px-4 py-3 position-fixed transition">
       <nav className="navbar nav-bar-main navbar-expand-lg rounded-2 ">
         <div className="container-fluid">
           <button
             className="border-0 bg-transparent d-block d-lg-none"
             onClick={() => props.setSidebarShow(false)}
           >
-              <img src="/images/svg/toggle.svg" alt="toggle" />
+            <img src="/images/svg/toggle.svg" alt="toggle" />
           </button>
           <h2 className="fw-600 d-none d-lg-block">Dashboard</h2>
           <Link className="d-block d-lg-none">
@@ -44,7 +42,32 @@ export default function Navbar(props) {
               <div className="profile-box d-flex gap-3 align-items-center justify-content-center">
                 <p className="fw-500">Liza Daniel</p>
                 <div className="profile-logo d-none d-lg-block">
-                  <img src="/images/profile-logo.png" alt="profile-logo" />
+                  <div class="dropdown">
+                    <button
+                      class="border-0 bg-transparent"
+                      type="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      <img
+                        src="/images/profile-logo.png"
+                        className="cursor-pointer"
+                        alt="profile-logo"
+                      />
+                    </button>
+                    <ul class="dropdown-menu">
+                      <li>
+                        <Link class="dropdown-item" to="/my-account">
+                          Profile
+                        </Link>
+                      </li>
+                      <li>
+                        <Link class="dropdown-item" to="">
+                          Log out
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./assets/sass/style.scss"
 import { Provider } from "react-redux";
 import { store } from "./redux/configureStore";
