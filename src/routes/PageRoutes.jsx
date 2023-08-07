33756b2eeb73ad576a350418c7c22cb5476ec1e9
manishp9Work <manishpandey1255work@gmail.com>
2023-08-07 18:33:23 +0530

    made some changes by manish

--- src/routes/PageRoutes.jsx
+++ src/routes/PageRoutes.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Login from "../pages/Login";
 import Dashboard from "../pages/Dashboard";
 import Instructors from "../pages/Instructors";
+import InstructorProfile from "../pages/InstructorProfile";
 
 export default function PageRoutes() {
   const routes = [
@@ -16,6 +17,10 @@ export default function PageRoutes() {
     {
       path: "/instructors",
       Component: Instructors,
+    },
+    {
+      path: "/instructors-profile",
+      Component: InstructorProfile,
     },
   ];
 
